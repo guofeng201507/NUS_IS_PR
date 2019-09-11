03d4610de8a10b58a7f63a576fd4b8bb58e8fd9d
guo feng <guofeng20094@gmail.com>
2019-09-12 00:34:47 +0800

    update

--- CA2/ca2_transfer_learning.py
+++ CA2/ca2_transfer_learning.py
@@ -7,21 +7,97 @@ base_model = ResNet50(weights='imagenet',
                       include_top=False,
                       input_shape=(HEIGHT, WIDTH, 3))
 
+base_model.summary()
 
 from keras.preprocessing.image import ImageDataGenerator
 
-TRAIN_DIR = "food_dataset"
+TRAIN_DIR = r"C:\Users\guofe\Downloads\CA2_Data"
 HEIGHT = 300
 WIDTH = 300
 BATCH_SIZE = 8
 
-train_datagen =  ImageDataGenerator(
-      preprocessing_function=preprocess_input,
-      rotation_range=90,
-      horizontal_flip=True,
-      vertical_flip=True
-    )
+train_datagen = ImageDataGenerator(
+    preprocessing_function=preprocess_input,
+    rotation_range=90,
+    horizontal_flip=True,
+    vertical_flip=True
+)
 
 train_generator = train_datagen.flow_from_directory(TRAIN_DIR,
                                                     target_size=(HEIGHT, WIDTH),
                                                     batch_size=BATCH_SIZE)
+
+from keras.layers import Dense, Activation, Flatten, Dropout
+from keras.models import Sequential, Model
+
+
+def build_finetune_model(base_model, dropout, fc_layers, num_classes):
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    x = base_model.output
+    x = Flatten()(x)
+    for fc in fc_layers:
+        # New FC layer, random init
+        x = Dense(fc, activation='relu')(x)
+        x = Dropout(dropout)(x)
+
+    # New softmax layer
+    predictions = Dense(num_classes, activation='softmax')(x)
+
+    finetune_model = Model(inputs=base_model.input, outputs=predictions)
+
+    return finetune_model
+
+
+class_list = ["food", "landmark", "person"]
+FC_LAYERS = [512, 512]
+dropout = 0.5
+
+finetune_model = build_finetune_model(base_model,
+                                      dropout=dropout,
+                                      fc_layers=FC_LAYERS,
+                                      num_classes=len(class_list))
+
+from keras.optimizers import SGD, Adam
+import matplotlib.pyplot as plt
+from keras.callbacks import ModelCheckpoint
+
+
+# Plot the training and validation loss + accuracy
+def plot_training(history):
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(len(acc))
+
+    plt.plot(epochs, acc, 'r.')
+    plt.plot(epochs, val_acc, 'r')
+    plt.title('Training and validation accuracy')
+
+    # plt.figure()
+    # plt.plot(epochs, loss, 'r.')
+    # plt.plot(epochs, val_loss, 'r-')
+    # plt.title('Training and validation loss')
+    plt.show()
+
+    plt.savefig('acc_vs_epochs.png')
+
+
+NUM_EPOCHS = 10
+BATCH_SIZE = 8
+num_train_images = 10000
+
+adam = Adam(lr=0.00001)
+finetune_model.compile(adam, loss='categorical_crossentropy', metrics=['accuracy'])
+
+filepath = "./checkpoints/" + "ResNet50" + "_model_weights.h5"
+checkpoint = ModelCheckpoint(filepath, monitor=["acc"], verbose=1, mode='max')
+callbacks_list = [checkpoint]
+
+history = finetune_model.fit_generator(train_generator, epochs=NUM_EPOCHS, workers=8,
+                                       steps_per_epoch=num_train_images // BATCH_SIZE,
+                                       shuffle=True, callbacks=callbacks_list)
+
+plot_training(history)
